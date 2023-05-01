9f6b0a84034ac2e6093d7de7f09037be584a9ab8
imac <frank.siyli@gmail.com>
2023-05-01 17:55:02 +0200

    zustand for president

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from "../components/Posts/postsSlice";
+import usersReducer from "../components/Users/usersSlice";
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
+    users: usersReducer,
   },
 });
